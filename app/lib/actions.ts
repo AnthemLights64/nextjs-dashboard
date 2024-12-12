4fc1d3e0304068fe4a492651094208c535122d62
wenlonglu <yummybroccoli@outlook.com>
2024-12-13 01:00:48 +0800

    解决删除Invoice操作中action的类型问题

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -102,11 +102,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    try {
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-        revalidatePath('/dashboard/invoices');
-        return { message: 'Deleted Invoice.' };
-    } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
